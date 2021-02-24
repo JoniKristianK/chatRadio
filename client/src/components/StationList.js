4f877aa2bf630712a64dd3290419fc194eb28136
JoniKristianK <joni.karilainen@gmail.com>
2021-02-24 08:24:31 +0200

    Add MUI styles

--- client/src/components/StationList.js
+++ client/src/components/StationList.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import Iframe from 'react-iframe';
 
+import { makeStyles } from '@material-ui/core/styles';
+
 import radioRock from '../assests/radioRock.webp';
 import radioCity from '../assests/radioCity.webp';
 import iskelma from '../assests/iskelma.webp';
@@ -12,10 +14,27 @@ import nrj from '../assests/nrj.webp';
 import groove from '../assests/groove.webp';
 import nova from '../assests/nova.webp';
 
-const StationList = () => {
-  const [station, setStation] = useState('s117163');
+const useStyles = makeStyles({
+  root: {},
+  stationListImg: {
+    padding: '5px',
+    border: 'solid black 2px',
+    margin: '5px',
+    borderRadius: '10px',
+  },
+  stations: {
+    width: '400px',
+    margin: '0 auto',
+  },
+  player: {
+    border: 'solid black 3px',
+    borderRadius: '13px',
+  },
+});
 
-  let stationURL = `https://tunein.com/embed/player/${station}/`;
+const StationList = () => {
+  const classes = useStyles();
+  const [station, setStation] = useState('s117163');
 
   const stationList = [
     { src: radioCity, alt: 'Radio City', id: 's14868' },
@@ -37,29 +56,24 @@ const StationList = () => {
       width='70'
       src={station.src}
       alt={station.alt}
-      style={{
-        padding: '5px',
-        border: 'solid black 2px',
-        margin: '5px',
-        borderRadius: '10px',
-      }}
       onClick={() => setStation(station.id)}
+      className={classes.stationListImg}
     />
   ));
 
+  const stationURL = `https://tunein.com/embed/player/${station}/`;
+
   return (
     <div>
-      <div style={{ width: '400px', margin: '0 auto' }}>{stations}</div>
-
-      <div style={{ border: 'solid black 3px', borderRadius: '13px' }}>
+      <div className={classes.stations}>{stations}</div>
+      <div className={classes.player}>
         <Iframe
           url={stationURL}
           width='500px'
           height='95px'
-          id='myId'
-          className='myClassname'
-          display='initial'
-          position='relative'
+          // display='initial'
+          // position='relative'
+          // className='myClassname'
         />
       </div>
     </div>
