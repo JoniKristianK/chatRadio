1acb6dbb9a69da2ffb7cd24ac782e5a98992bdaa
JoniKristianK <joni.karilainen@gmail.com>
2021-03-03 05:53:16 +0200

    Edit MenuItem colors

--- client/src/components/SelectGenre.js
+++ client/src/components/SelectGenre.js
@@ -7,16 +7,19 @@ import Select from '@material-ui/core/Select';
 
 import { localStations, rockMetal, electric } from '../dbStations';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles({
   formControl: {
-    margin: theme.spacing(1),
-    minWidth: 300,
+    margin: '0px 0px 30px 0px',
+    minWidth: '370px',
+    border: 'solid cyan 2px',
+    borderRadius: '10px',
   },
-}));
+});
 
 const SelectGenre = ({ handleGenreList }) => {
   const classes = useStyles();
-  const [genre, setGenre] = useState('');
+
+  const [genre, setGenre] = useState(localStations);
 
   const handleChange = (event) => {
     setGenre(event.target.value);
@@ -26,19 +29,34 @@ const SelectGenre = ({ handleGenreList }) => {
   return (
     <div>
       <FormControl className={classes.formControl}>
-        <InputLabel id='label'>
-          <strong>Select Genre</strong>
-        </InputLabel>
+        {/* <InputLabel id='label'>Select Genre</InputLabel> */}
         <Select
-          labelId='label'
+          style={{ backgroundColor: 'black', color: 'cyan', padding: '7px' }}
+          // labelId='label'
           id='select'
           value={genre}
           onChange={handleChange}
         >
           {/* <em>Select</em> */}
-          <MenuItem value={localStations}>Local Stations</MenuItem>
-          <MenuItem value={rockMetal}>Rock / Metal</MenuItem>
-          <MenuItem value={electric}>Electric</MenuItem>
+
+          <MenuItem
+            style={{ backgroundColor: 'black', color: 'cyan' }}
+            value={localStations}
+          >
+            Local Stations
+          </MenuItem>
+          <MenuItem
+            style={{ backgroundColor: 'black', color: 'cyan' }}
+            value={rockMetal}
+          >
+            Rock / Metal
+          </MenuItem>
+          <MenuItem
+            style={{ backgroundColor: 'black', color: 'cyan' }}
+            value={electric}
+          >
+            Electric
+          </MenuItem>
         </Select>
       </FormControl>
     </div>
