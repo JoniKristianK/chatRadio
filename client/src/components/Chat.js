3f628b5b6d3c43e8995eefc62901de5fec350836
JoniKristianK <joni.karilainen@gmail.com>
2021-03-09 15:59:37 +0200

    Add Favorites and Chat components

---
+++ client/src/components/Chat.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Chat = () => {
+  return (
+    <div style={{ height: '310px' }}>
+      <div>
+        <hr />
+        CHAT CHAT CHAT CHAT
+        <hr />
+        CHAT CHAT CHAT CHAT
+        <hr />
+        CHAT CHAT CHAT CHAT
+        <hr />
+        CHAT CHAT CHAT CHAT
+        <hr />
+        CHAT CHAT CHAT CHAT
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
