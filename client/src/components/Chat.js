c39aa8698e7e38828aeb08e09cd59871539a4faf
JoniKristianK <joni.karilainen@gmail.com>
2021-03-12 08:40:17 +0200

    Add Chat component

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,22 +1,50 @@
 import React from 'react';
 
-const Chat = () => {
+import '../chat.css';
+import useChat from '../useChat';
+
+const ChatRoom = (props) => {
+  const { roomId } = props.match.params; // Gets roomId from URL
+  const { messages, sendMessage } = useChat(roomId); // Creates a websocket and manages messaging
+  const [newMessage, setNewMessage] = React.useState(''); // Message to be sent
+
+  const handleNewMessageChange = (event) => {
+    setNewMessage(event.target.value);
+  };
+
+  const handleSendMessage = () => {
+    sendMessage(newMessage);
+    setNewMessage('');
+  };
+
   return (
-    <div style={{ height: '310px' }}>
-      <div>
-        <hr />
-        CHAT CHAT CHAT CHAT
-        <hr />
-        CHAT CHAT CHAT CHAT
-        <hr />
-        CHAT CHAT CHAT CHAT
-        <hr />
-        CHAT CHAT CHAT CHAT
-        <hr />
-        CHAT CHAT CHAT CHAT
+    <div className='chat-room-container'>
+      <h1 className='room-name'>Room: {roomId}</h1>
+      <div className='messages-container'>
+        <ol className='messages-list'>
+          {messages.map((message, i) => (
+            <li
+              key={i}
+              className={`message-item ${
+                message.ownedByCurrentUser ? 'my-message' : 'received-message'
+              }`}
+            >
+              {message.body}
+            </li>
+          ))}
+        </ol>
       </div>
+      <textarea
+        value={newMessage}
+        onChange={handleNewMessageChange}
+        placeholder='Write message...'
+        className='new-message-input-field'
+      />
+      <button onClick={handleSendMessage} className='send-message-button'>
+        Send
+      </button>
     </div>
   );
 };
 
-export default Chat;
+export default ChatRoom;
