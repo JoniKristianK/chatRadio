3ceb9157a20af329c65361f9e46142a6a7e932f4
JoniKristianK <joni.karilainen@gmail.com>
2021-03-31 19:37:42 +0300

    Add: routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import ToolBar from './components/ToolBar';
 import Player from './components/Player';
+import ChatRadio from './components/ChatRadio';
 import Footer from './components/Footer';
 
 import './App.css';
+// import 'index.css';
 
 function App() {
   return (
@@ -13,7 +16,12 @@ function App() {
       <header className='App-header'>
         <h1>ChatRadio</h1>
         <div className='App-container'>
-          <Player />
+          <Router>
+            <Switch>
+              <Route exact path='/' component={Player} />
+              <Route exact path='/:roomId' component={ChatRadio} />
+            </Switch>
+          </Router>
         </div>
       </header>
       <Footer />
